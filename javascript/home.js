7d6afda7fdc8c8736dbede442f1c5aeb22a2dd16
Cyrel <cyrelyumang10@gmail.com>
2023-05-08 16:38:13 +0800

    POPUP HOME SECTION

--- javascript/home.js
+++ javascript/home.js
@@ -1,2 +1,12 @@
-const hide_card =  document.querySelector('.profile-card');
-const buttonCloseHome = document.querySelector('.home-close-button');
+const hide_card = document.querySelector(".profile-card-hidden");
+const buttonCloseHome = document.querySelector(".home-close-button");
+const buttonClickMe = document.querySelector(".home__data");
+
+buttonCloseHome.addEventListener("click", () => {
+  hide_card.classList.replace("profile-card", "profile-card-hidden");
+});
+
+buttonClickMe.addEventListener("click", () => {
+    console.log("Click");
+  hide_card.classList.replace("profile-card-hidden", "profile-card");
+});
