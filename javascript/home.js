b5b4104dead62939adf8bae2925f7c03a7b0ea99
Cyrel <cyrelyumang10@gmail.com>
2023-05-21 19:09:56 +0800

    fingerprint update

--- javascript/home.js
+++ javascript/home.js
@@ -1,12 +1,35 @@
+// const hide_card = document.querySelector(".profile-card-hidden");
+// const buttonCloseHome = document.querySelector(".home-close-button");
+// const buttonClickMe = document.querySelector(".home__fingerprint");
+
+// buttonCloseHome.addEventListener("click", () => {
+//   hide_card.classList.replace("profile-card", "profile-card-hidden");
+// });
+
+// buttonClickMe.addEventListener("click", () => {
+//   hide_card.classList.replace("profile-card-hidden", "profile-card");
+// });
+
+// AUTO POPUP PROFILE
+
 const hide_card = document.querySelector(".profile-card-hidden");
 const buttonCloseHome = document.querySelector(".home-close-button");
 const buttonClickMe = document.querySelector(".home__fingerprint");
+let intervalTimer;
+
+buttonClickMe.addEventListener("mouseover", () => { 
+  intervalTimer = setInterval(popUpProfile, 1000);
+});
+
+buttonClickMe.addEventListener("mouseout", () => {
+  clearInterval(intervalTimer);
+  console.log("MOUSE OUT");
+});
 
 buttonCloseHome.addEventListener("click", () => {
   hide_card.classList.replace("profile-card", "profile-card-hidden");
 });
 
-buttonClickMe.addEventListener("click", () => {
-    console.log("Click");
+function popUpProfile() {
   hide_card.classList.replace("profile-card-hidden", "profile-card");
-});
+}
